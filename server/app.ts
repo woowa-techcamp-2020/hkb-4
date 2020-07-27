a08a06e18231b903e7084e760969ba02a8608711
eunjung <jenny.it.kr@gmail.com>
2020-07-28 01:12:25 +0900

    익스프레스 기본 미들웨어 설정
    - helmet : 보안
    - morgan : 로그
    - cors : cors 이슈 (추후에 디테일한 설정 필요)
    - body-parser : json 데이터 해석
    - postman 으로 테스트 진행

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,6 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("hello world"));
+app.post("/", (req, res) => res.send(`received data is "${req.body.data}"`));
 
 app.listen(PORT, () => console.log(`✅ server is running on port: ${PORT}`));
