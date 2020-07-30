7819700a16a9866008db9dcfb35d7544c2683aa5
sooojungee <sooojungee@gmail.com>
2020-07-30 14:07:42 +0900

    login page 마크업
    login page 마크업 작업, 스타일 적용
    app에 붙임

--- client/src/views/app.ts
+++ client/src/views/app.ts
@@ -1,12 +1,15 @@
 import Header from './header';
+import LoginPage from './login';
 
 class App {
 	private app!: HTMLElement;
 	constructor(target: HTMLElement) {
 		this.app = target;
 		const header = new Header();
-		//
 		this.app.appendChild(header);
+		//
+		const loginPage = new LoginPage();
+		this.app.appendChild(loginPage);
 	}
 }
 
