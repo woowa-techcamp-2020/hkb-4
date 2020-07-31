2cfe10bfbd5d185c54074cac05d8b3a91693f987
sooojungee <sooojungee@gmail.com>
2020-07-31 18:13:16 +0900

    hkb 파일 생성 및 연동
    공통적으로 쓰는 scss 변수를 담는 scss 파일을 만듦
    app.ts에 hkb를 붙임

---
+++ client/src/views/hkb/hkb.ts
@@ -0,0 +1,20 @@
+import NavigationBar from '../navigation';
+
+class Hkb extends HTMLElement {
+	private navigation!: HTMLElement;
+
+	constructor() {
+		super();
+		this.navigation = new NavigationBar();
+	}
+
+	connectedCallback() {
+		this.render();
+		this.appendChild(this.navigation);
+	}
+
+	render() {}
+}
+
+customElements.define('hkb-page', Hkb);
+export default customElements.get('hkb-page');
