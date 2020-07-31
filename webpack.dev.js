fe43e998546a6441dec5a933e3cad91f77c124b7
eunjung <jenny.it.kr@gmail.com>
2020-07-31 10:55:15 +0900

    webpack-dev-server 설정
    - client/app.ts 16~28 : 테스트코드

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,11 @@ const development = {
 	...config,
 	mode: 'development',
 	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		port: 8080,
+		hot: true,
+	},
 };
 
 module.exports = development;
