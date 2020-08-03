6d646946213c79e0c5f718a32e9d1276e84f9cc3
eunjung <jenny.it.kr@gmail.com>
2020-08-03 14:08:26 +0900

    일별 상세 내역 스타일 수정

--- client/src/views/header/header.ts
+++ client/src/views/header/header.ts
@@ -11,7 +11,7 @@ class Header extends HTMLElement {
 		this.innerHTML = `
       <div class="flex-1"></div>
       <div class="header">가계부</div>
-      <span class="flex-1 user-name">@헤더가 흰색 아래 부분에 배경색있음좋겠ㅇ염</span>
+      <span class="flex-1 user-name">유저네임</span>
     `;
 	}
 }
