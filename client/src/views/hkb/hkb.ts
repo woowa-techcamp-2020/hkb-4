e5eabecf231aa2a42f83bb86c1ed8c898a3bae52
sooojungee <sooojungee@gmail.com>
2020-08-03 13:34:48 +0900

    barchart animation 추가하는 중

--- client/src/views/hkb/hkb.ts
+++ client/src/views/hkb/hkb.ts
@@ -1,16 +1,19 @@
-import NavigationBar from '../navigation';
+// import NavigationBar from '../navigation';
+import ChartsTab from '../charts';
 
 class Hkb extends HTMLElement {
 	private navigation!: HTMLElement;
-
+	private chartsTab!: HTMLElement;
 	constructor() {
 		super();
-		this.navigation = new NavigationBar();
+		// this.navigation = new NavigationBar();
+		this.chartsTab = new ChartsTab();
 	}
 
 	connectedCallback() {
 		this.render();
-		this.appendChild(this.navigation);
+		// this.appendChild(this.navigation);
+		this.appendChild(this.chartsTab);
 	}
 
 	render() {}
