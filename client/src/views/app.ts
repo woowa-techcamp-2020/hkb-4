78b414ec99d03eeef120b7b3b8d7fa9557403c0b
eunjung <jenny.it.kr@gmail.com>
2020-08-03 16:47:33 +0900

    app.ts 메서드 복구

--- client/src/views/app.ts
+++ client/src/views/app.ts
@@ -16,5 +16,25 @@ class App {
 
 		this.init();
 	}
+	init() {
+		this.app.appendChild(new Header());
+		this.observer.subscribe('userChanged', this, this.render.bind(this));
+		this.userController.initUser();
+	}
+
+	render(user: object) {
+		if (this.app.childElementCount === 2) {
+			this.app.removeChild(this.app.lastChild);
+		}
+		if (Object.keys(user).length) {
+			const appContent = document.createElement('div');
+			appContent.setAttribute('id', 'app-content');
+			this.app.appendChild(appContent);
+			appContent.appendChild(new HkbPage());
+		} else {
+			this.app.appendChild(new LoginPage());
+		}
+	}
+}
 
 export default App;
