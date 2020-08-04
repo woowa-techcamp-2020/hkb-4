434b74fb353559629e57c8ab70533accd32f7ad0
eunjung <jenny.it.kr@gmail.com>
2020-08-04 23:42:26 +0900

    ledger 입력창 핸들링 구현
    - 수입/지출 버튼 토글
    - 분류에 따른 카테고리 변경
    - 내용 지우기/삭제 버튼 토글
    - 날짜, 카테고리, 결ㅈ수단 기본 옵션 설정
    - 거래 내역 클릭시 인풋창에 내용 주입 구현

--- client/src/controller/hkbController.ts
+++ client/src/controller/hkbController.ts
@@ -21,7 +21,7 @@ class HkbController {
 		if (item) {
 			this.handleItemEdit(item);
 		} else if (radioButton) {
-			this.handleTypeSelection(radioButton);
+			this.toggleTypeButton(radioButton);
 		} else if (submitButton) {
 			this.handleItemSubmit(submitButton);
 		} else if (initButton) {
@@ -34,19 +34,73 @@ class HkbController {
 	}
 
 	handleItemEdit(item) {
-		console.log('handleItemEdit');
+		const inputContainer = item.closest('hkb-ledger').querySelector('.container-input');
+		this.fillInput(item, inputContainer);
 	}
 
-	handleTypeSelection(button) {
+	changeSelectOption(type, item) {
+		const optionsContainer = document.querySelector(`select[name="${type}"`);
+		const options = optionsContainer.querySelectorAll('option');
+		options.forEach(option => {
+			if (option.value === item[type]) {
+				option.setAttribute('selected', '');
+			}
+		});
+	}
+
+	fillInput(item, inputContainer) {
+		inputContainer.querySelector('.init-button').classList.add('hide');
+		inputContainer.querySelector('.delete-button').classList.remove('hide');
+		const checkedRadioButton = inputContainer.querySelector(`input[value="${item.type}"]`);
+		checkedRadioButton.checked = true;
+		this.toggleTypeButton(checkedRadioButton);
+		// @ts-ignore
+		inputContainer.querySelector('input[name="amount"]').value = item.amount;
+		//@ts-ignore
+		inputContainer.querySelector('input[name="description"]').value = item.description;
+		//@ts-ignore
+		inputContainer.querySelector('input[type="date"]').value = '2020-01-01';
+		this.changeSelectOption('category', item);
+		this.changeSelectOption('payment', item);
+	}
+
+	toggleTypeButton(button) {
 		const labels = button.closest('.group').querySelectorAll('label');
 		labels.forEach(label => {
 			const radio = label.querySelector('input');
 			if (radio.checked) {
 				label.classList.add('button--active');
+				this.changeCategory(radio.value);
 			} else {
 				label.classList.remove('button--active');
 			}
 		});
+	}
+
+	changeCategory(category) {
+		const spendingCategory = [
+			'식비',
+			'생활',
+			'쇼핑/뷰티',
+			'교통',
+			'의료/건강',
+			'문화/여가',
+			'미지정',
+		];
+		const incomeCategory = ['월급', '용돈', '기타수입'];
+		const categorySelect = document.querySelector('select[name="category"]');
+		if (category === '수입') {
+			categorySelect.innerHTML = incomeCategory.reduce(
+				(prev, next) => prev + `<option value="${next}">${next}</option>`,
+				'',
+			);
+		} else {
+			categorySelect.innerHTML = spendingCategory.reduce(
+				(prev, next) => prev + `<option value="${next}">${next}</option>`,
+				'',
+			);
+		}
+		categorySelect.firstElementChild.setAttribute('selected', '');
 	}
 
 	handleItemSubmit(button) {
--- client/src/views/ledger/ledger.ts
+++ client/src/views/ledger/ledger.ts
@@ -22,41 +22,46 @@ class Ledger extends HTMLElement {
         <div class="group">
           <span>분류</span>
           <label for="type-income" class="button income-button">수입
-            <input id="type-income" type="radio" name="type" value="1"/>
+            <input id="type-income" type="radio" name="type" value="수입"/>
           </label>
           <label for="type-spending" class="button spending-button button--active">지출
-            <input id="type-spending" type="radio" name="type" value="2">
+            <input id="type-spending" type="radio" name="type" value="지출">
           </label>
         </div>
       </div>
       <div class="row">
         <div class="group">
           <span>날짜</span>
-          <input type="date" />
+          <input type="date" value="2020-08-04"/>
         </div>
         <div class="group">
           <span>카테고리</span>
-          <select
-            ><option value="기타">기타</option
-            ><option value="교육비">교육비</option></select
-          >
+          <select name="category">
+            <option value="식비" selected>식비</option>
+            <option value="생활">생활</option>
+            <option value="쇼핑/뷰티">쇼핑/뷰티</option>
+            <option value="교통">교통</option>
+            <option value="의료/건강">의료/건강</option>
+            <option value="문화/여가">문화/여가</option>
+            <option value="미분류">미분류</option>
+          </select>
         </div>
         <div class="group">
           <span>결제수단</span>
-          <select
-            ><option value="하나카드">하나카드</option
-            ><option value="삼성카드">삼성카드</option>
+          <select name="payment">
+            <option value="하나카드" selected>하나카드</option>
+            <option value="삼성카드">삼성카드</option>
           </select>
         </div>
       </div>
       <div class="row">
         <div class="group">
           <span>금액</span>
-          <input type="text" />
+          <input type="text" name="amount" />
         </div>
         <div class="group">
           <span>내용</span>
-          <input type="text" "/>
+          <input type="text" name="description"/>
         </div>
       </div>
       <div class="row">
