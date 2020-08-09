7551af51a12d043f696850d463ce457181121226
eunjung <jenny.it.kr@gmail.com>
2020-08-10 00:22:28 +0900

    Ledger setDate 함수 날짜 포맷 버그 수정
    - 10 초과에서 10 이상으로 수정

--- client/src/views/ledger/ledger.ts
+++ client/src/views/ledger/ledger.ts
@@ -42,8 +42,8 @@ class Ledger extends HTMLElement {
 		const year = today.getFullYear();
 		const month = today.getMonth() + 1;
 		const date = today.getDate();
-		const todayString = `${year}-${month > 10 ? month : `0${month}`}-${
-			date > 10 ? date : `0${date}`
+		const todayString = `${year}-${month >= 10 ? month : `0${month}`}-${
+			date >= 10 ? date : `0${date}`
 		}`;
 		// @ts-ignore
 		document.querySelector('input[name="date"]').value = todayString;
