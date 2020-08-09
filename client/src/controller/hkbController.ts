4065976970fb2b3f89af6bcbe9d0edf88e76b88d
eunjung <jenny.it.kr@gmail.com>
2020-08-10 00:48:24 +0900

    input 초기화 함수 날짜 포맷 버그 수정
    - 10 초과에서 10 이상으로 수정

--- client/src/controller/hkbController.ts
+++ client/src/controller/hkbController.ts
@@ -317,8 +317,8 @@ class HkbController {
 		const year = today.getFullYear();
 		const month = today.getMonth() + 1;
 		const date = today.getDate();
-		const todayToString = `${year}-${month > 10 ? month : `0${month}`}-${
-			date > 10 ? date : `0${date}`
+		const todayToString = `${year}-${month >= 10 ? month : `0${month}`}-${
+			date >= 10 ? date : `0${date}`
 		}`;
 		const dateInput = document.querySelector('input[name="date"]');
 		// @ts-ignore
